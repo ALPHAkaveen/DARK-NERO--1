00c1a873ad871e977690f2aa7d017088273c0744
KAVEESHA SITHUM <107849120+Kaveeshasithum@users.noreply.github.com>
2022-07-13 11:50:14 +0530

    ✫ ✫ ✫𝙳𝙰𝚁𝙺 𝙽𝙴𝚁𝙾✫ ✫ ✫

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '©_KAVEESHA.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/Kaveeshasithum/DARK-NERO-.git' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
